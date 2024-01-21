b3f933a0ad5125244d70eed6beea156d4875e65c
Imedia device <imediadevice@Imedias-MacBook-Pro.local>
2024-01-21 17:13:19 +0100

    dashboard

--- heart_health_tracking/__Screens/DashBoard/DashBoard.tsx
+++ heart_health_tracking/__Screens/DashBoard/DashBoard.tsx
@@ -1,17 +1,41 @@
-import { useState } from 'react'
-import { SafeAreaView, Text, View } from 'react-native'
+import {useState} from 'react';
+import {Image, SafeAreaView, Text, TouchableOpacity, View} from 'react-native';
+import { Bars4Icon, UserCircleIcon, ChevronDoubleDownIcon, ChevronDoubleUpIcon, CalendarDaysIcon, ChartBarSquareIcon } from "react-native-heroicons/solid"
 
 const DashBoard = () => {
   return (
-    <SafeAreaView>
-        <View className='flex flex-col flex-1'>
-            <View className='flex'>
-
-            </View>
-            <View className='flex-1'></View>
-            <View></View>
+    <SafeAreaView className="flex">
+      <View className="flex  dark:bg-gray-900 dark:text-gray-200  text-black ">
+        <View className={`flex flex-row justify-between items-center `}>
+          <View className={``}>
+            <TouchableOpacity>
+            <Bars4Icon
+                    color={'black'}
+                    className="dark:text-white text-black"
+                    size={35}
+                  />
+            </TouchableOpacity>
+          </View>
+          <View className={` flex-1 justify-center items-center `}>
+            <TouchableOpacity className="">
+              <Image
+                source={require('../../__Assets/SysPhotos/haert-logo.png')}
+                className={`object-contain`}
+              />
+            </TouchableOpacity>
+          </View>
+          <View className={``}>
+            <TouchableOpacity>
+            <UserCircleIcon color="black" size={35} />
+            </TouchableOpacity>
+          </View>
         </View>
+        <View className={`!flex-1 bg-gray-700`}>
+          <Text>BODY</Text>
+        </View>
+        <View className={``}></View>
+      </View>
     </SafeAreaView>
-  )
-}
-export default DashBoard
+  );
+};
+export default DashBoard;
--- heart_health_tracking/tailwind.config.js
+++ heart_health_tracking/tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./screens/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  content: ["./App.{js,jsx,ts,tsx}", "./__Screens/**/*.{js,jsx,ts,tsx}", "./__Components/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      backgroundImage:{
-        "Wall_Back":"url('./assets/images/bgWhite.png')"
-      },
+
     },
   },
   darkMode: "class",
